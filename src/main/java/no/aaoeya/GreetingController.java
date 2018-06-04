bf73f9811d932508f3a275a3317239fc11f360eb
anol <aeolsen@gmail.com>
2018-06-04 11:19:31 +0200

    Endret pakkenavnet til aaoeya

--- src/main/java/no/aaoeya/GreetingController.java
+++ src/main/java/no/aaoeya/GreetingController.java
@@ -1,4 +1,4 @@
-package hello;
+package no.aaoeya;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +13,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Åøya") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
